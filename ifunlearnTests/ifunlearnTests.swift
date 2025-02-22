c019a2be99f0e829095a5976d1e1dadbce1bc8a9
Abdulmenaf Altintas <aaltintas@Abdulmenafs-MacBook-Pro.local>
2025-02-21 22:11:02 -0500

    Initial Commit

---
+++ ifunlearnTests/ifunlearnTests.swift
@@ -0,0 +1,16 @@
+//
+//  ifunlearnTests.swift
+//  ifunlearnTests
+//
+//  Created by Abdulmenaf Altintas on 2025-02-21.
+//
+
+import Testing
+
+struct ifunlearnTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
